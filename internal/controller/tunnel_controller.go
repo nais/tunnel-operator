b8f33909eb1a616e42250c6a1bea47993f711306
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2026-04-22 14:48:34 +0200

    feat: create PolicyException in team namespace for gateway debug mode

--- internal/controller/tunnel_controller.go
+++ internal/controller/tunnel_controller.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net"
 	"net/http"
@@ -17,8 +18,11 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/intstr"
 
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -47,6 +51,7 @@ type TunnelReconciler struct {
 //+kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch;create;delete
 //+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch
 //+kubebuilder:rbac:groups=networking.k8s.io,resources=networkpolicies,verbs=get;list;watch;create;delete
+//+kubebuilder:rbac:groups=kyverno.io,resources=policyexceptions,verbs=get;list;watch;create;delete
 
 func (r *TunnelReconciler) Reconcile(ctx context.Context, req ctrl.Request) (result ctrl.Result, retErr error) {
 	defer func() {
@@ -171,6 +176,18 @@ func (r *TunnelReconciler) Reconcile(ctx context.Context, req ctrl.Request) (res
 		}
 		if err := r.Client.Create(ctx, pod); err != nil {
 			return ctrl.Result{}, fmt.Errorf("creating gateway pod: %w", err)
+		}
+
+		if gatewayDebug {
+			polex := newPolicyException(resourceName, tunnel.Namespace)
+			if err := controllerutil.SetControllerReference(tunnel, polex, r.Scheme); err != nil {
+				return ctrl.Result{}, fmt.Errorf("setting owner reference on policy exception: %w", err)
+			}
+			if err := r.Client.Create(ctx, polex); err != nil {
+				if !apierrors.IsAlreadyExists(err) && !isNoMatchError(err) {
+					return ctrl.Result{}, fmt.Errorf("creating policy exception: %w", err)
+				}
+			}
 		}
 	}
 
@@ -357,9 +374,14 @@ func (r *TunnelReconciler) deleteGatewayResources(ctx context.Context, tunnel *v
 		&networkingv1.NetworkPolicy{ObjectMeta: metav1.ObjectMeta{Name: resourceName, Namespace: tunnel.Namespace}},
 	}
 
+	polex := newPolicyException(resourceName, tunnel.Namespace)
+	objects = append(objects, polex)
+
 	for _, obj := range objects {
-		if err := client.IgnoreNotFound(r.Client.Delete(ctx, obj)); err != nil {
-			return fmt.Errorf("deleting %T: %w", obj, err)
+		if err := r.Client.Delete(ctx, obj); err != nil {
+			if !apierrors.IsNotFound(err) && !isNoMatchError(err) {
+				return fmt.Errorf("deleting %T: %w", obj, err)
+			}
 		}
 	}
 
@@ -469,6 +491,41 @@ func gatewayCapabilities(debug bool) *corev1.Capabilities {
 	return caps
 }
 
+func newPolicyException(name, namespace string) *unstructured.Unstructured {
+	polex := &unstructured.Unstructured{}
+	polex.SetGroupVersionKind(schema.GroupVersionKind{
+		Group:   "kyverno.io",
+		Version: "v2",
+		Kind:    "PolicyException",
+	})
+	polex.SetName(name)
+	polex.SetNamespace(namespace)
+	polex.Object["spec"] = map[string]any{
+		"match": map[string]any{
+			"any": []any{
+				map[string]any{
+					"resources": map[string]any{
+						"kinds":      []any{"Pod"},
+						"names":      []any{name},
+						"namespaces": []any{namespace},
+					},
+				},
+			},
+		},
+		"exceptions": []any{
+			map[string]any{
+				"policyName": "disallow-capabilities",
+				"ruleNames":  []any{"adding-capabilities", "autogen-adding-capabilities"},
+			},
+			map[string]any{
+				"policyName": "disallow-capabilities-strict",
+				"ruleNames":  []any{"adding-capabilities-strict", "autogen-adding-capabilities-strict"},
+			},
+		},
+	}
+	return polex
+}
+
 func intstrPtr(i int32) *intstr.IntOrString {
 	v := intstr.FromInt32(i)
 	return &v
@@ -482,6 +539,12 @@ func isPodReady(pod *corev1.Pod) bool {
 	}
 
 	return false
+}
+
+func isNoMatchError(err error) bool {
+	var noKind *meta.NoKindMatchError
+	var noResource *meta.NoResourceMatchError
+	return errors.As(err, &noKind) || errors.As(err, &noResource)
 }
 
 func (r *TunnelReconciler) SetupWithManager(mgr ctrl.Manager) error {
